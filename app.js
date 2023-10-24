bc20e14e0fa78ca3c75f232f7c1c9206282daf94
Anuj kushwaha <anujkushwaha664@gmail.com>
2023-10-25 00:35:59 +0530

    test cases handled

--- app.js
+++ app.js
@@ -68,33 +68,81 @@ dot.addEventListener("click", function(){
     box1.innerText = expression;
 });
 pluse.addEventListener("click", function(){
-    expression = expression + "+"; 
-    box1.innerText = expression;
+    
+    if(expression.length == 0){
+        alert("please enter a valid expression");
+        expression = "";
+        box1.innerText = expression;
+        return;
+    }
+    let flag= check(expression, '+');
+    if(flag){
+         expression = expression + "+";
+         box1.innerText = expression;
+    }
 });
 minus.addEventListener("click", function(){
-    expression = expression + "-"; 
-    box1.innerText = expression;
+    
+    if(expression.length == 0){
+        alert("please enter a valid expression");
+        expression = "";
+        box1.innerText = expression;
+        return;
+    }
+    let flag= check(expression, '-');
+    if(flag){
+         expression = expression + "-";
+         box1.innerText = expression;
+    }
 });
 multiply.addEventListener("click", function(){
-    expression = expression + "*"; 
-    box1.innerText = expression;
+    
+    if(expression.length == 0){
+        alert("please enter a valid expression");
+        expression = "";
+        box1.innerText = expression;
+        return;
+    }
+    let flag= check(expression, '*');
+    if(flag){
+         expression = expression + "*";
+         box1.innerText = expression;
+    }
 });
 divide.addEventListener("click", function(){
-    expression = expression + "/"; 
-    box1.innerText = expression;
+   
+    if(expression.length == 0){
+        alert("please enter a valid expression");
+        expression = "";
+        box1.innerText = expression;
+        return;
+    } 
+    let flag= check(expression, '/');
+    if(flag){
+         expression = expression + "/";
+         box1.innerText = expression;
+    }
 });
 modulo.addEventListener("click", function(){
     expression = expression + "/100"; 
     expression = eval(expression);
     box1.innerText = expression;
+    
 });
 Ac.addEventListener("click", function(){
     expression = ""; 
     box1.innerText = expression;
 });
 equ.addEventListener("click", function(){
-    expression = removeDuplicates(expression);
+    let n = expression.length-1;
+    if(n==-1 || expression[n]=='+' || expression[n]=='-' || expression[n]=='*' || expression[n]=='/'){
+        alert("please enter a valid expression");
+        expression = "";
+        box1.innerText = expression;
+        return;
+    } 
     expression = eval(expression); 
+
     box1.innerText = expression;
 });
 plusMinus.addEventListener("click", function(){
@@ -104,23 +152,23 @@ plusMinus.addEventListener("click", function(){
             flag = false;
         }
     }
-    if(flag == true){
-        expression = '-' + expression; 
-        expression = eval(expression);
+    if(flag == true || expression.charAt(0)=='-'){
+        if(expression.charAt(0)=='-'){
+            expression = expression.substring(1,expression.length);
+        }else{
+            expression = '-' + expression;
+        }
         box1.innerText = expression;
     }
     
 });
 
-function removeDuplicates( expression ){
-    let newString = "";
-    for(let i=0; i<expression.length-1; i++){
-        if(expression[i] != expression[i+1])
-        {
-            newString += expression[i];
-        }
+function check(exp, special){
+    let i = exp.length-1;
+    if(exp[i] <'1' || exp[i] >'9'){
+        alert("please enter a valid expression");   
+        return false;
     }
-
-    newString += expression[expression.length-1];
-    return newString;
+    return true;
 }
+
